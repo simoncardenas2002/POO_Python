1a88d118f1fd7c2a05f8197fbd53ff2e0c528c15
Simon Cardenas Duque <simon-cardenasduque@hotmail.com>
2023-02-15 17:30:23 -0500

    agregando archivos

---
+++ abstraccion.py
@@ -0,0 +1,25 @@
+class Lavadora:
+    def __init__(self):
+        pass
+
+    def lavar(self,temperatura="caliente"):
+        self._llenar_tanque(temperatura)
+        self._añadir_jabon()
+        self._proceso()
+        self._centrifugar()
+
+    def _llenar_tanque(self,temperatura):
+        print(f"llenando el tanque con agua {temperatura}")
+
+    def _añadir_jabon(self):
+        print("Añadiendo jabon")
+
+    def _proceso(self):
+        print("lavando")
+
+    def _centrifugar(self):
+        print("Centrifugando la ropa")
+
+if __name__ == '__main__':
+  lavadora=Lavadora()
+  lavadora.lavar()
